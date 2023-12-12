d7025b776447413992af98eac02f6711ee7bd410
Giftson <giftson@Giftsons-MacBook-Air.local>
2023-12-13 03:05:36 +0800

    login error design

--- client/src/routes/login/index.tsx
+++ client/src/routes/login/index.tsx
@@ -9,9 +9,11 @@ import { useDispatch } from "react-redux";
 import { setUser } from "features/user/userSlice";
 
 function Login() {
+  // error message
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
-  const from = location?.state?.from?.pathname || "/";
   // tokens
   const { setItem: setAccessTokenItem } = useLocalStorage(
     LocalStorageEnum.ACCESS_TOKEN
@@ -20,9 +22,8 @@ function Login() {
     LocalStorageEnum.REFRESH_TOKEN
   );
   const dispatch = useDispatch();
-  // error message
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+
+  const from = location?.state?.from?.pathname || "/";
 
   const onLoginSuccess = async (codeResponse: { code: string }) => {
     const res = await fetch("http://localhost:8000/api/auth/login", {
@@ -70,10 +71,15 @@ function Login() {
   };
   return (
     <div className="flex h-screen w-screen justify-center">
+      {error && (
+        <div className="flex fixed top-0 w-screen justify-center p-6">
+          <div className="p-3 rounded bg-red-400 text-gray-200">{error}</div>
+        </div>
+      )}
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <div className="flex items-center ">
+        <div className="flex items-center">
           <div className="flex flex-col items-center w-auto h-fit rounded-md p-16 bg-gray-800 hover:bg-gray-300  transition-all duration-300 ease-linear group">
             <button
               onClick={() => googleLogin()}
